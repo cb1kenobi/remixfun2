fd72881b6f0fc80419f2040fcc448cbea0082d21
Chris Barber <chris.barber@vercel.com>
2023-09-28 08:32:49 -0500

    Initial commit

--- app/routes/myedge.tsx
+++ app/routes/myedge.tsx
@@ -1,20 +1,22 @@
 import type { MetaFunction } from "@vercel/remix";
 
+export const config = { runtime: 'edge' };
+
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "My Edge" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
-export default function Index() {
+export default function MyEdge() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Home</h1>
+      <h1>My Edge Page</h1>
       <ul>
         <li>
-          <a href="/myedge">
-            My Edge
+          <a href="/">
+            Home
           </a>
         </li>
       </ul>
